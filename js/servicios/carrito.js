f09f05ea4cb470ef1de3d105f02896f476968ac3
yazmin nuñez <yazzyramone@gmail.com>
2022-01-29 19:32:34 -0300

    cree los archivos de carrito y productos pertenecientes al  servicio

---
+++ js/servicios/carrito.js
@@ -0,0 +1,16 @@
+class CarritoService{
+
+    URL_carrito = 'https://61e6d10ace3a2d001735948a.mockapi.io/Carrito/'
+
+    async GuardarCarrito(carrito){
+        let CARRITO_GUARDADO= await http.post(this.URL_carrito,carrito)
+        return CARRITO_GUARDADO
+    }
+
+
+
+
+
+}
+
+const CARRITO_SERVICE = new CarritoService()
