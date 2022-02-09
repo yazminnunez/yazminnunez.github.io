f87801340707ebe379dbd0daea0d470379220ad1
yazmin nuñez <yazzyramone@gmail.com>
2022-02-09 17:12:02 -0300

    ingresando los enviroments para que se conecte a distintos tipos de enviroments,configuracion avanzada

--- config.js
+++ config.js
@@ -1,6 +1,16 @@
+import dotenv from 'dotenv'
+
+//dotenv.config()
+
+dotenv.config({
+  //  path:'miconfig.env'
+    path: process.env.NODE_ENV +'.env'
+})
+
+
 export default{
-    port:8080,
-    tipo_persistencia:'MONGODB',
-    STR_CNX:'mongodb+srv://jazminsORG:O12R34Gorg@cluster0.jsxov.mongodb.net/ecommerce?retryWrites=true&w=majority',
+    port:process.env.port || 8080,
+    tipo_persistencia:process.env.tipo_p ||'MONGODB',
+    STR_CNX:process.env.CNX ||'mongodb+srv://jazminsORG:O12R34Gorg@cluster0.jsxov.mongodb.net/ecommerce?retryWrites=true&w=majority',
     //const STR_CNX ='mongodb://localhost/ecommerce'
 }
